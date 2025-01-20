60a6e8e6c03de61f7c6ad3ff0216289a958bdd40
harsh sukhija <harsh.sukhija.31.1@mountblue.tech>
2025-01-20 10:13:18 +0530

    feat:added not found page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./HomePage";
 import CountryDetail from "./CountryDetail";
+import PageNotFound from "./PageNotFound";
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/:countryName" element={<CountryDetail />} />
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/PageNotFound.jsx
@@ -0,0 +1,5 @@
+const PageNotFound = () => {
+  return <div className="flex justify-center mt-14"> 404 PageNotFound</div>;
+};
+
+export default PageNotFound;
